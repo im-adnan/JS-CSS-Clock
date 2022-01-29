b0e77f7bd90b4366b6b665983e4902b87b32cdc4
im-adnan <sayedmuhammadadnan@gmail.com>
2022-01-29 23:44:53 +0530

    Segregated

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const deg = 6;
-const hr = document.querySelector("hr");
-const mn = document.querySelector("mn");
-const sc = document.querySelector("sc");
-setInterval(( ) => {
-    const day = new Date('noite');
-    const hh = day.getHours() * 30;
-    const mm = day.getMinutes() * deg;
-    const ss = day.getSeconds() * deg;
-    hr.style.transform = `rotatez(${(hh)+(mm/12)}deg)`;
-    mn.style.transform = `rotatez(${mm}deg)`;
-    sc.style.transform = `rotatez(${ss}deg)`;
-},1000);
+const hr = document.querySelector("#hr");
+const mn = document.querySelector("#mn");
+const sc = document.querySelector("#sc");
+setInterval(() =>{
+let day =new Date();
+let hh = day.getHours() *30;
+let mm = day.getMinutes()*deg;
+let ss = day.getSeconds()*deg;
+hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
+mn.style.transform = `rotateZ(${mm}deg)`;
+sc.style.transform = `rotateZ(${ss}deg)`;
+})
